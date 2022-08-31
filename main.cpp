f72a0188ba12b1f3d3d2cceec399a053de32f2c6
zhongqi1112 <zhongqi1112@gmail.com>
2022-08-31 01:08:48 -0700

    first commit

---
+++ main.cpp
@@ -0,0 +1,51 @@
+/**
+ * @author:
+ * @project:
+ * @purpose:
+ * @date:
+ */
+
+#include <iostream>
+
+//------------------------------------------------------------------------------
+//Files we are testing:
+#include "includes/stub/stub.h"
+
+
+
+//------------------------------------------------------------------------------
+
+using namespace std;
+
+void basic_test(bool debug=false);
+void testB(bool debug=false);
+
+int main()
+{
+    // BASIC TEST
+    cout << "\n\n---------running basic_test---------\n\n";
+    basic_test();
+    cout << "\n\n------------- D O N E --------------\n\n";
+
+    // TEST B
+    cout << "\n\n-----------running testB------------\n\n";
+    testB();
+    cout << "\n\n------------- D O N E --------------\n\n";
+
+    return 0;
+}
+
+
+void basic_test(bool debug)
+{
+    if (stub()) {
+        cout << "Passed!" << endl;
+    } else {
+        cout << "Failed!" << endl;
+    }
+}
+
+void testB(bool debug)
+{
+    // TODO: add more test here
+}
